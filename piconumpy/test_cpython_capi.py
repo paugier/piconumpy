e2083e00f2faea20b5405a9f2a5580471164dbb6
paugier <pierre.augier@univ-grenoble-alpes.fr>
2020-03-08 15:40:11 +0100

    Reorganize tests

--- piconumpy/test_cpython_capi.py
+++ piconumpy/test_cpython_capi.py
@@ -1,37 +1,36 @@
+
 import numpy as np
 
 from . import array
 
 
-def test_init_array():
-    a = array([1.0, 2.0])
-    assert a.size == 2
+class Tests:
+    _array = array
 
+    def test_init_array(self):
+        a = self._array([1.0, 2.0])
+        assert a.size == 2
 
-def test_init_array_numpy():
-    np_a = np.array([1.0, 2.0, 0.0, 0.0])
-    a = array(np_a.tolist())
-    assert a.size == np_a.size
-    assert a.tolist() == np_a.tolist()
+    def test_init_array_numpy(self):
+        np_a = np.array([1.0, 2.0, 0.0, 0.0])
+        a = self._array(np_a.tolist())
+        assert a.size == np_a.size
+        assert a.tolist() == np_a.tolist()
 
+    def test_multiply(self):
+        a = self._array([1.0, 2.0])
+        assert (2 * a).tolist() == [2.0, 4.0]
+        assert (a * 3).tolist() == [3.0, 6.0]
 
-def test_multiply():
-    a = array([1.0, 2.0])
-    assert (2 * a).tolist() == [2.0, 4.0]
-    assert (a * 3).tolist() == [3.0, 6.0]
+    def test_add(self):
+        a = self._array([1.0, 2.0])
+        assert (a + 2 * a).tolist() == [3.0, 6.0]
 
+    def test_divide(self):
+        a = self._array([1.0, 2.0])
+        assert (a / 2).tolist() == [0.5, 1.0]
 
-def test_add():
-    a = array([1.0, 2.0])
-    assert (a + 2 * a).tolist() == [3.0, 6.0]
-
-
-def test_divide():
-    a = array([1.0, 2.0])
-    assert (a / 2).tolist() == [0.5, 1.0]
-
-
-def test_sequence():
-    a = array([1.0, 2.0])
-    assert len(a) == 2
-    assert a[1] == 2.0
+    def test_sequence(self):
+        a = self._array([1.0, 2.0])
+        assert len(a) == 2
+        assert a[1] == 2.0
--- piconumpy/test_purepy.py
+++ piconumpy/test_purepy.py
@@ -1,37 +1,6 @@
-import numpy as np
-
 from .purepy import array
+from .test_cpython_capi import Tests as _Tests
 
 
-def test_init_array():
-    a = array([1.0, 2.0])
-    assert a.size == 2
-
-
-def test_init_array_numpy():
-    np_a = np.array([1.0, 2.0, 0.0, 0.0])
-    a = array(np_a.tolist())
-    assert a.size == np_a.size
-    assert a.tolist() == np_a.tolist()
-
-
-def test_multiply():
-    a = array([1.0, 2.0])
-    assert (2 * a).tolist() == [2.0, 4.0]
-    assert (a * 3).tolist() == [3.0, 6.0]
-
-
-def test_add():
-    a = array([1.0, 2.0])
-    assert (a + 2 * a).tolist() == [3.0, 6.0]
-
-
-def test_divide():
-    a = array([1.0, 2.0])
-    assert (a / 2).tolist() == [0.5, 1.0]
-
-
-def test_sequence():
-    a = array([1.0, 2.0])
-    assert len(a) == 2
-    assert a[1] == 2.0
+class Tests(_Tests):
+    _array = array
